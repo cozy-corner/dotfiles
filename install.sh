41b79d8bdf011854c5734371674ec2064ed5dc0f
cozy-corner <koji.sasaki@sansan.com>
2024-04-27 09:38:59 +0900

    add sl

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.ssh ~/.ssh
+ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 
