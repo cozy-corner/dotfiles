18e792b0eda7914416e530224eda2a0d74dbee58
cozy-corner <koji.sasaki@sansan.com>
2024-12-31 14:35:31 +0900

    add ghostty settings

--- install.sh
+++ install.sh
@@ -6,5 +6,7 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.ssh ~/.ssh
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
-ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/ghostty/config ~/.config/ghostty/config
+ln -sf ~/dotfiles/ghostty/themes ~/.config/ghostty/themes
 
